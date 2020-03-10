e58a4869c2b7dd580cd0185b634cbe4e33624d0b
lewiscampbell <41307157+lewiscampbell@users.noreply.github.com>
2020-03-10 12:15:08 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ if (!spaceId || !accessToken) {
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Contentful starter',
+    title: 'lewsthoughts',
   },
   pathPrefix: '/gatsby-contentful-starter',
   plugins: [
