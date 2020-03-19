585527a69215a13cd655c9631c47ae87fd22d999
lewiscampbell <lew.campbell1@gmail.com>
2020-03-19 12:25:27 +0000

    nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,14 +3,17 @@ import { Link } from 'gatsby'
 import styles from './navigation.module.css'
 
 export default () => (
-  <nav role="navigation">
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
-      </li>
-    </ul>
-  </nav>
+  <header>
+    <h1 class="title">lewsthoughts</h1>
+    <nav role="navigation">
+      <ul className={styles.navigation}>
+        <li className={styles.navigationItem}>
+          <Link to="/">Home</Link>
+        </li>
+        <li className={styles.navigationItem}>
+          <Link to="/blog/">Blog</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 )
