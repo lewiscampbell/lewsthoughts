c6f3bfb92c4b819298661ee9b79125e40e107d5b
lewiscampbell <lew.campbell1@gmail.com>
2020-03-24 17:35:32 +0000

    deploy fix test 1

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,11 +4,7 @@ import Img from 'gatsby-image'
 import styles from './hero.module.css'
 
 export default ({ data }) => (
-  <div className={styles.hero}>
-    <Img
-      className={styles.heroImage}
-      alt={data.name}
-      fluid={data.heroImage.fluid}
-    />
+  <div>
+
   </div>
 )
