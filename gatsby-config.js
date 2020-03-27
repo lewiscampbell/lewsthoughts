1a068170b4b1ce25b61c91a784b0dfda4cd3e306
Lewis Campbell <lew.campbell1@gmail.com>
2020-03-27 10:26:07 +0000

    adding google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,5 +27,12 @@ module.exports = {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-157437563-1",
+      },
+    },
   ],
 }
