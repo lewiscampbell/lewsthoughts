572b729cf4b523d21f7ab85e1cc41914e416be57
Lewis Campbell <lew.campbell1@gmail.com>
2020-03-27 10:22:54 +0000

    delete google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,15 +29,3 @@ module.exports = {
     },
   ],
 }
-
-module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
-        trackingId: "UA-157437563-1",
-      },
-    },
-  ],
-}
