389939f024aed5720370b38262933d952b979e61
lewiscampbell <lew.campbell1@gmail.com>
2020-04-02 09:42:53 +0100

    site title

---
+++ src/pages/interviews.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Layout from '../components/layout'
+import styles from './interviews.module.css'
+
+export default () => (
+    <Layout>
+        <div style={{ background: '#fff' }}>
+            <div className={styles.heroBlog}></div>
+            <div className="wrapper">
+                <h2 className="section-headline">Interviews</h2>
+                <ul className="article-list">
+
+                </ul>
+            </div>
+        </div>
+    </Layout>
+)
