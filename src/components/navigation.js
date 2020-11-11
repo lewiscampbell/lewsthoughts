b4664c82092755c5805f60af6590dedd093c71ca
Lewis Campbell <lewiscampbell@Lewiss-iMac.lan>
2020-11-11 15:02:03 +0000

    favicon change + lew talks pg

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,9 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 import styles from './navigation.module.css'
 
+
+
+
+
 export default () => (
   <header>
     <h1 class="title">Lew's Thoughts</h1>
+
     <nav role="navigation">
       <ul className={styles.navigation}>
         <li className={styles.navigationItem}>
@@ -13,7 +18,11 @@ export default () => (
         <li className={styles.navigationItem}>
           <Link to="/blog/" className={styles.link}>Blog</Link>
         </li>
+        <li className={styles.navigationItem}>
+          <Link to="/lews_talks/" className={styles.link}>Lew's Talks</Link>
+        </li>
       </ul>
     </nav>
+
   </header>
 )
--- src/pages/lews_talks.js
+++ src/pages/lews_talks.js
@@ -7,10 +7,14 @@ export default () => (
         <div style={{ background: '#fff' }}>
             <div className={styles.heroBlog}></div>
             <div className="wrapper">
-                <h2 className="section-headline">Interviews</h2>
+            <div class=""><iframe src="https://mixlr.com/users/8268414/embed" width="100%" height="180px" scrolling="no" frameborder="no" marginheight="0"></iframe></div>
+            <br></br>
+                <h2 className="section-headline">Episodes Coming Soon</h2>
                 <ul className="article-list">
 
                 </ul>
+
+                
             </div>
         </div>
     </Layout>
